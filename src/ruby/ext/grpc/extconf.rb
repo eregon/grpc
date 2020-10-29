85ad3a8869d38a5cfb08c77637a349bb62af27cd
Benoit Daloze <eregontp@gmail.com>
2020-10-29 18:03:28 +0100

    Link dynamically to libgrpc

--- src/ruby/ext/grpc/extconf.rb
+++ src/ruby/ext/grpc/extconf.rb
@@ -61,7 +61,9 @@ unless windows
   nproc = 4
   nproc = Etc.nprocessors * 2 if Etc.respond_to? :nprocessors
   make = bsd ? 'gmake' : 'make'
-  system("#{make} -j#{nproc} -C #{grpc_root} #{grpc_lib_dir}/libgrpc.a CONFIG=#{grpc_config} Q=")
+  libgrpc_so = "#{grpc_lib_dir}/libgrpc.#{RbConfig::CONFIG['SOEXT']}.13.0.0"
+  system("#{make} -j#{nproc} -C #{grpc_root} #{libgrpc_so} CONFIG=#{grpc_config} Q=")
+
   exit 1 unless $? == 0
 end
 
@@ -71,7 +73,9 @@ ext_export_file = File.join(grpc_root, 'src', 'ruby', 'ext', 'grpc', 'ext-export
 $LDFLAGS << ' -Wl,--version-script="' + ext_export_file + '.gcc"' if RUBY_PLATFORM =~ /linux/
 $LDFLAGS << ' -Wl,-exported_symbols_list,"' + ext_export_file + '.clang"' if RUBY_PLATFORM =~ /darwin/
 
-$LDFLAGS << ' ' + File.join(grpc_lib_dir, 'libgrpc.a') unless windows
+# $LDFLAGS << ' ' + File.join(grpc_lib_dir, 'libgrpc.a') unless windows
+$LDFLAGS << " -L#{grpc_lib_dir} -rpath #{grpc_lib_dir} -lgrpc"
+
 if grpc_config == 'gcov'
   $CFLAGS << ' -O0 -fprofile-arcs -ftest-coverage'
   $LDFLAGS << ' -fprofile-arcs -ftest-coverage -rdynamic'
